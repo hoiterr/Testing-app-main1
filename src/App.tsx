3d7e3465179b3cb4159e4a011da859f6b181022b
hoiterr <algimantas.s@protonmail.com>
2025-08-19 12:54:27 +0300

    letsgo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 
 
 import React, { lazy, Suspense } from 'react';
-import { AnalysisProvider } from './contexts/AnalysisContext';
-import { ChatProvider } from './contexts/ChatContext';
-import { HistoryProvider } from './contexts/HistoryContext';
-import { UIProvider } from './contexts/UIContext';
-import { AppProvider } from './contexts/AppContext';
-import { ErrorProvider } from './contexts/ErrorContext';
-import { OnboardingProvider } from './contexts/OnboardingContext';
-import { ThemeProvider } from './contexts/ThemeContext'; // New import
-import AppLayout from './components/layouts/AppLayout';
-import { logService } from './services/logService';
+import { AnalysisProvider } from '@/contexts/AnalysisContext';
+import { ChatProvider } from '@/contexts/ChatContext';
+import { HistoryProvider } from '@/contexts/HistoryContext';
+import { UIProvider } from '@/contexts/UIContext';
+import { AppProvider } from '@/contexts/AppContext';
+import { ErrorProvider } from '@/contexts/ErrorContext';
+import { OnboardingProvider } from '@/contexts/OnboardingContext';
+import { ThemeProvider } from '@/contexts/ThemeContext'; // New import
+import AppLayout from '@/components/layouts/AppLayout';
+import { logService } from '@/services/logService';
 
-const DebugConsole = lazy(() => import('./components/ui/DebugConsole').then(module => ({ default: module.DebugConsole })));
+const DebugConsole = lazy(() => import('@/components/ui/DebugConsole').then(module => ({ default: module.DebugConsole })));
 
 interface AppProps {}
 
